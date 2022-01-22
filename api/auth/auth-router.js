80d3aa3509684a9e2ff171fd885af90e02561002
ChrisJRamirez <chrisjamesramirez@gmail.com>
2022-01-21 20:06:49 -0500

    added imports to auth-router, built basic jokes-model with db access functions

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,4 +1,7 @@
 const router = require('express').Router();
+const {JWT_SECRET, BCRYPT_ROUNDS} = require("../secrets/index");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 router.post('/register', (req, res) => {
   res.end('implement register, please!');
